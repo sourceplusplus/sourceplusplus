098eb318e5cce2bd09761a0e87f42134ecb07e7a
Brandon Fergerson <bfergerson@apache.org>
2021-09-22 12:28:11 -0400

    fix serialization
    Former-commit-id: 7266fb0375e0b626b92703b9f18480f5eb031df6

--- protocol/src/main/kotlin/spp/protocol/probe/command/LiveInstrumentCommand.kt
+++ protocol/src/main/kotlin/spp/protocol/probe/command/LiveInstrumentCommand.kt
@@ -2,19 +2,20 @@ package spp.protocol.probe.command
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.beans.ConstructorProperties
+import java.io.Serializable
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class LiveInstrumentCommand @ConstructorProperties("commandType", "context") constructor(
     var commandType: CommandType,
     var context: LiveInstrumentContext
-) {
+) : Serializable {
 
     data class Response(
         var isSuccess: Boolean,
         var fault: String? = null,
         var timestamp: Long,
         var context: LiveInstrumentContext
-    )
+    ) : Serializable
 
     enum class CommandType {
         GET_LIVE_INSTRUMENTS,
