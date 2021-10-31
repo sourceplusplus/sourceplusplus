227f3d81f1b69796d6556ca15476bb7dc2298272
Brandon Fergerson <bfergerson@apache.org>
2021-10-31 09:29:15 -0400

    bump skywalking

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,10 +44,10 @@ tasks {
 
     register("downloadSkywalking") {
         doLast {
-            val f = File(projectDir, "docker/e2e/apache-skywalking-apm-es7-$skywalkingVersion.tar.gz")
+            val f = File(projectDir, "docker/e2e/apache-skywalking-apm-$skywalkingVersion.tar.gz")
             if (!f.exists()) {
                 println("Downloading Apache SkyWalking")
-                URL("https://archive.apache.org/dist/skywalking/$skywalkingVersion/apache-skywalking-apm-es7-$skywalkingVersion.tar.gz")
+                URL("https://downloads.apache.org/skywalking/$skywalkingVersion/apache-skywalking-apm-$skywalkingVersion.tar.gz")
                     .openStream().use { input ->
                         FileOutputStream(f).use { output ->
                             input.copyTo(output)
