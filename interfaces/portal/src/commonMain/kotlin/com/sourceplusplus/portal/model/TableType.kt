19a34905777282480921a517a042288d8cbc8542
Brandon Fergerson <bfergerson@apache.org>
2021-11-10 11:30:57 -0500

    move portal
    Former-commit-id: 2ab5ca7e68951623ac3c2ba8f2cf03e4cb2e3efb

---
+++ interfaces/portal/src/commonMain/kotlin/com/sourceplusplus/portal/model/TableType.kt
@@ -0,0 +1,12 @@
+package com.sourceplusplus.portal.model
+
+/**
+ * todo: description.
+ *
+ * @since 0.1.0
+ * @author [Brandon Fergerson](mailto:bfergerson@apache.org)
+ */
+interface TableType {
+    val description: String
+    val isCentered: Boolean
+}
