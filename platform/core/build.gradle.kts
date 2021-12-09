dda1d863f2b537390227d4d04689ed20b82678a2
Brandon Fergerson <bfergerson@apache.org>
2021-12-09 14:15:10 -0500

    test

--- platform/core/build.gradle.kts
+++ platform/core/build.gradle.kts
@@ -59,7 +59,7 @@ graal {
     outputName("spp-platform")
     option("-H:+PrintClassInitialization")
     option("-H:+ReportExceptionStackTraces")
-    option("-H:+TraceClassInitialization")
+//    option("-H:+TraceClassInitialization")
     option("-H:IncludeResourceBundles=build")
 }
 
