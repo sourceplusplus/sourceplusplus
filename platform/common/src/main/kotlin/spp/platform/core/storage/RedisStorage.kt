1aadfd222c2ba5ad32686c6315d82b4a235a3290
Brandon Fergerson <bfergerson@apache.org>
2022-01-06 17:01:41 -0500

    mem storage

--- platform/common/src/main/kotlin/spp/platform/core/storage/RedisStorage.kt
+++ platform/common/src/main/kotlin/spp/platform/core/storage/RedisStorage.kt
@@ -1,59 +1,59 @@
 package spp.platform.core.storage
 
+import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.coroutines.await
+import io.vertx.redis.client.Redis
 import io.vertx.redis.client.RedisAPI
-import org.apache.commons.lang3.RandomStringUtils
 import spp.platform.core.SourceStorage
 import spp.protocol.auth.*
 import spp.protocol.developer.Developer
 import java.nio.charset.StandardCharsets
 
-class RedisStorage {
+class RedisStorage : CoreStorage {
 
     lateinit var redis: RedisAPI
 
-    suspend fun getDevelopers(): List<Developer> {
+    suspend fun init(vertx: Vertx, config: JsonObject) {
+        val sdHost = System.getenv("SPP_REDIS_HOST") ?: config.getJsonObject("redis").getString("host")
+        val sdPort = System.getenv("SPP_REDIS_PORT") ?: config.getJsonObject("redis").getInteger("port")
+        val redisClient = Redis.createClient(vertx, "redis://$sdHost:$sdPort").connect().await()
+        redis = RedisAPI.api(redisClient)
+    }
+
+    override suspend fun getDevelopers(): List<Developer> {
         val devIds = redis.smembers("developers:ids").await()
         return devIds.map { Developer(it.toString(StandardCharsets.UTF_8)) }
     }
 
-    suspend fun getDeveloperByAccessToken(token: String): Developer? {
+    override suspend fun getDeveloperByAccessToken(token: String): Developer? {
         val devId = redis.get("developers:access_tokens:$token").await()
             ?.toString(StandardCharsets.UTF_8) ?: return null
         return Developer(devId)
     }
 
-    suspend fun hasAccessToken(token: String): Boolean {
-        return redis.sismember("developers:access_tokens", token).await().toBoolean()
-    }
-
-    suspend fun hasRole(roleName: String): Boolean {
+    override suspend fun hasRole(roleName: String): Boolean {
         val role = DeveloperRole.fromString(roleName)
         return redis.sismember("roles", role.roleName).await().toBoolean()
     }
 
-    suspend fun removeRole(role: DeveloperRole): Boolean {
+    override suspend fun removeRole(role: DeveloperRole): Boolean {
         getRolePermissions(role).forEach {
             removePermissionFromRole(role, it)
         }
         return redis.srem(listOf("roles", role.roleName)).await().toBoolean()
     }
 
-    suspend fun addRole(roleName: String): Boolean {
+    override suspend fun addRole(roleName: String): Boolean {
         val role = DeveloperRole.fromString(roleName)
         return redis.sadd(listOf("roles", role.roleName)).await().toBoolean()
     }
 
-    suspend fun hasDeveloper(id: String): Boolean {
+    override suspend fun hasDeveloper(id: String): Boolean {
         return redis.sismember("developers:ids", id).await().toBoolean()
     }
 
-    suspend fun addDeveloper(id: String): Developer {
-        return addDeveloper(id, RandomStringUtils.randomAlphanumeric(50))
-    }
-
-    suspend fun addDeveloper(id: String, token: String): Developer {
+    override suspend fun addDeveloper(id: String, token: String): Developer {
         redis.sadd(listOf("developers:ids", id)).await()
         redis.set(listOf("developers:access_tokens:$token", id)).await()
         redis.sadd(listOf("developers:access_tokens", token)).await()
@@ -62,7 +62,7 @@ class RedisStorage {
         return Developer(id, token)
     }
 
-    suspend fun removeDeveloper(id: String) {
+    override suspend fun removeDeveloper(id: String) {
         val accessToken = getAccessToken(id)
         redis.srem(listOf("developers:ids", id)).await()
         redis.del(listOf("developers:access_tokens:$accessToken")).await()
@@ -71,11 +71,11 @@ class RedisStorage {
         redis.del(listOf("developers:$id:roles")).await()
     }
 
-    suspend fun getAccessToken(id: String): String {
+    private suspend fun getAccessToken(id: String): String {
         return redis.get("developers:ids:$id:access_token").await().toString(StandardCharsets.UTF_8)
     }
 
-    suspend fun setAccessToken(id: String, accessToken: String) {
+    override suspend fun setAccessToken(id: String, accessToken: String) {
         //remove existing token
         val existingToken = redis.get("developers:ids:$id:access_token").await()
         if (existingToken != null) {
@@ -97,26 +97,26 @@ class RedisStorage {
         redis.set(listOf("developers:ids:$id:access_token", accessToken)).await()
     }
 
-    suspend fun getDeveloperRoles(developerId: String): List<DeveloperRole> {
+    override suspend fun getDeveloperRoles(developerId: String): List<DeveloperRole> {
         return redis.smembers("developers:$developerId:roles").await()
             .map { DeveloperRole.fromString(it.toString(StandardCharsets.UTF_8)) }
     }
 
-    suspend fun getRoleAccessPermissions(role: DeveloperRole): Set<AccessPermission> {
+    override suspend fun getRoleAccessPermissions(role: DeveloperRole): Set<AccessPermission> {
         val accessPermissions = redis.smembers("roles:${role.roleName}:access_permissions").await()
         return accessPermissions.map { getAccessPermission(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
 
-    suspend fun getAccessPermissions(): Set<AccessPermission> {
+    override suspend fun getAccessPermissions(): Set<AccessPermission> {
         val accessPermissions = redis.smembers("access_permissions").await()
         return accessPermissions.map { getAccessPermission(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
 
-    suspend fun hasAccessPermission(id: String): Boolean {
+    override suspend fun hasAccessPermission(id: String): Boolean {
         return redis.sismember("access_permissions", id).await().toBoolean()
     }
 
-    suspend fun getAccessPermission(id: String): AccessPermission {
+    override suspend fun getAccessPermission(id: String): AccessPermission {
         val accessPermissions = redis.get("access_permissions:$id").await()
         val dataObject = JsonObject(accessPermissions.toString(StandardCharsets.UTF_8))
         return AccessPermission(
@@ -126,7 +126,7 @@ class RedisStorage {
         )
     }
 
-    suspend fun addAccessPermission(id: String, locationPatterns: List<String>, type: AccessType) {
+    override suspend fun addAccessPermission(id: String, locationPatterns: List<String>, type: AccessType) {
         redis.sadd(listOf("access_permissions", id)).await()
         redis.set(
             listOf(
@@ -139,7 +139,7 @@ class RedisStorage {
         ).await()
     }
 
-    suspend fun removeAccessPermission(id: String) {
+    override suspend fun removeAccessPermission(id: String) {
         getRoles().forEach {
             removeAccessPermissionFromRole(id, it)
         }
@@ -147,24 +147,24 @@ class RedisStorage {
         redis.del(listOf("access_permissions:$id")).await()
     }
 
-    suspend fun addAccessPermissionToRole(id: String, role: DeveloperRole) {
+    override suspend fun addAccessPermissionToRole(id: String, role: DeveloperRole) {
         redis.sadd(listOf("roles:${role.roleName}:access_permissions", id)).await()
     }
 
-    suspend fun removeAccessPermissionFromRole(id: String, role: DeveloperRole) {
+    override suspend fun removeAccessPermissionFromRole(id: String, role: DeveloperRole) {
         redis.srem(listOf("roles:${role.roleName}:access_permissions", id)).await()
     }
 
-    suspend fun getDataRedactions(): Set<DataRedaction> {
+    override suspend fun getDataRedactions(): Set<DataRedaction> {
         val roles = redis.smembers("data_redactions").await()
         return roles.map { getDataRedaction(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
 
-    suspend fun hasDataRedaction(id: String): Boolean {
+    override suspend fun hasDataRedaction(id: String): Boolean {
         return redis.sismember("data_redactions", id).await().toBoolean()
     }
 
-    suspend fun getDataRedaction(id: String): DataRedaction {
+    override suspend fun getDataRedaction(id: String): DataRedaction {
         val permission = redis.get("data_redactions:$id").await()
         return DataRedaction(
             id,
@@ -172,12 +172,12 @@ class RedisStorage {
         )
     }
 
-    suspend fun addDataRedaction(id: String, redactionPattern: String) {
+    override suspend fun addDataRedaction(id: String, redactionPattern: String) {
         redis.sadd(listOf("data_redactions", id)).await()
         redis.set(listOf("data_redactions:$id", redactionPattern)).await()
     }
 
-    suspend fun removeDataRedaction(id: String) {
+    override suspend fun removeDataRedaction(id: String) {
         getRoles().forEach {
             removeDataRedactionFromRole(id, it)
         }
@@ -185,42 +185,42 @@ class RedisStorage {
         redis.del(listOf("data_redactions:$id")).await()
     }
 
-    suspend fun addDataRedactionToRole(id: String, role: DeveloperRole) {
+    override suspend fun addDataRedactionToRole(id: String, role: DeveloperRole) {
         redis.sadd(listOf("roles:${role.roleName}:data_redactions", id)).await()
     }
 
-    suspend fun removeDataRedactionFromRole(id: String, role: DeveloperRole) {
+    override suspend fun removeDataRedactionFromRole(id: String, role: DeveloperRole) {
         redis.srem(listOf("roles:${role.roleName}:data_redactions", id)).await()
     }
 
-    suspend fun getRoleDataRedactions(role: DeveloperRole): Set<DataRedaction> {
+    override suspend fun getRoleDataRedactions(role: DeveloperRole): Set<DataRedaction> {
         val dataRedactions = redis.smembers("roles:${role.roleName}:data_redactions").await()
         return dataRedactions.map { getDataRedaction(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
 
-    suspend fun getRoles(): Set<DeveloperRole> {
+    override suspend fun getRoles(): Set<DeveloperRole> {
         val roles = redis.smembers("roles").await()
         return roles.map { DeveloperRole.fromString(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
 
-    suspend fun addRoleToDeveloper(id: String, role: DeveloperRole) {
+    override suspend fun addRoleToDeveloper(id: String, role: DeveloperRole) {
         redis.sadd(listOf("developers:$id:roles", role.roleName)).await()
     }
 
-    suspend fun removeRoleFromDeveloper(id: String, role: DeveloperRole) {
+    override suspend fun removeRoleFromDeveloper(id: String, role: DeveloperRole) {
         redis.srem(listOf("developers:$id:roles", role.roleName)).await()
     }
 
-    suspend fun addPermissionToRole(role: DeveloperRole, permission: RolePermission) {
+    override suspend fun addPermissionToRole(role: DeveloperRole, permission: RolePermission) {
         redis.sadd(listOf("roles", role.roleName)).await()
         redis.sadd(listOf("roles:${role.roleName}:permissions", permission.name)).await()
     }
 
-    suspend fun removePermissionFromRole(role: DeveloperRole, permission: RolePermission) {
+    override suspend fun removePermissionFromRole(role: DeveloperRole, permission: RolePermission) {
         redis.srem(listOf("roles:${role.roleName}:permissions", permission.name)).await()
     }
 
-    suspend fun getRolePermissions(role: DeveloperRole): Set<RolePermission> {
+    override suspend fun getRolePermissions(role: DeveloperRole): Set<RolePermission> {
         val permissions = redis.smembers("roles:${role.roleName}:permissions").await()
         return permissions.map { RolePermission.valueOf(it.toString(StandardCharsets.UTF_8)) }.toSet()
     }
--- platform/core/src/main/kotlin/spp/platform/SourcePlatform.kt
+++ platform/core/src/main/kotlin/spp/platform/SourcePlatform.kt
@@ -37,8 +37,6 @@ import io.vertx.ext.web.handler.graphql.GraphQLHandler
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.await
 import io.vertx.kotlin.coroutines.dispatcher
-import io.vertx.redis.client.Redis
-import io.vertx.redis.client.RedisAPI
 import io.vertx.servicediscovery.Record
 import io.vertx.servicediscovery.ServiceDiscovery
 import io.vertx.servicediscovery.ServiceDiscoveryOptions
@@ -59,6 +57,7 @@ import spp.platform.core.SourceService
 import spp.platform.core.SourceServiceDiscovery
 import spp.platform.core.SourceStorage
 import spp.platform.core.storage.MemoryStorage
+import spp.platform.core.storage.RedisStorage
 import spp.platform.marker.MarkerTracker
 import spp.platform.marker.MarkerVerticle
 import spp.platform.probe.ProbeTracker
@@ -362,11 +361,23 @@ class SourcePlatform : CoroutineVerticle() {
         router["/stats"].handler(this::getStats)
         router["/clients"].handler(this::getClients)
 
-        log.info("Connecting to storage")
-//        val sdHost = System.getenv("SPP_REDIS_HOST") ?: config.getJsonObject("redis").getString("host")
-//        val sdPort = System.getenv("SPP_REDIS_PORT") ?: config.getJsonObject("redis").getInteger("port")
-//        val redisClient = Redis.createClient(vertx, "redis://$sdHost:$sdPort").connect().await()
-//        redis = RedisAPI.api(redisClient)
+        //Setup storage
+        when (val storageSelector = config.getJsonObject("storage").getString("selector")) {
+            "memory" -> {
+                log.info("Using in-memory storage")
+                SourceStorage.setup(MemoryStorage(vertx), config)
+            }
+            "redis" -> {
+                log.info("Using Redis storage")
+                val redisStorage = RedisStorage()
+                redisStorage.init(vertx, config)
+                SourceStorage.setup(redisStorage, config)
+            }
+            else -> {
+                log.error("Unknown storage selector: $storageSelector")
+                throw IllegalArgumentException("Unknown storage selector: $storageSelector")
+            }
+        }
 
         log.info("Starting service discovery")
         discovery = ServiceDiscovery.create(
@@ -458,8 +469,6 @@ class SourcePlatform : CoroutineVerticle() {
             .listen(httpPort, config.getJsonObject("spp-platform").getString("host")).await()
         vertx.sharedData().getLocalMap<String, Int>("spp.core")["http.port"] = server.actualPort()
         log.info("API server started. Port: {}", server.actualPort())
-
-        SourceStorage.setup(MemoryStorage(vertx), config)
         log.debug("Source++ Platform initialized")
     }
 
