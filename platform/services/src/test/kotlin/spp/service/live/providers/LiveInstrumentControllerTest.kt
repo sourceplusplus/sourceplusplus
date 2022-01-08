72ff8988ef991d81f810e9796cc74dcc91564bd1
Brandon Fergerson <bfergerson@apache.org>
2022-01-07 22:33:13 -0500

    move test

--- platform/services/src/test/kotlin/spp/service/live/providers/LiveInstrumentControllerTest.kt
+++ platform/services/src/test/kotlin/spp/service/live/providers/LiveInstrumentControllerTest.kt
@@ -1,20 +1,16 @@
 package spp.service.live.providers
 
-import com.google.common.io.Resources
-import spp.protocol.instrument.LiveSourceLocation
-import spp.protocol.instrument.LiveVariable
-import spp.protocol.instrument.breakpoint.LiveBreakpoint
 import io.vertx.core.Vertx
-import io.vertx.core.json.JsonObject
 import io.vertx.junit5.VertxExtension
 import io.vertx.junit5.VertxTestContext
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import spp.protocol.instrument.LiveSourceLocation
+import spp.protocol.instrument.breakpoint.LiveBreakpoint
 import spp.protocol.probe.ProbeAddress
 
 @ExtendWith(VertxExtension::class)
@@ -45,46 +41,4 @@ class LiveInstrumentControllerTest {
             }
         }
     }
-
-//    @Test
-//    fun processBpHit1() {
-//        val bpData = JsonObject(
-//            Resources.toString(Resources.getResource("bphit1.json"), Charsets.UTF_8)
-//        )
-//        val bpHit = LiveInstrumentController.transformRawBreakpointHit(bpData)
-//        assertNotNull(bpHit)
-//
-//        val topStack = bpHit.stackTrace.first()
-//        assertEquals(2, topStack.variables.size)
-//
-//        val thisVar = topStack.variables[0]
-//        assertEquals("this", thisVar.name)
-//        assertEquals(3, (thisVar.value as List<*>).size)
-//
-//        val s2Var = topStack.variables[1]
-//        assertEquals("s2", s2Var.name)
-//        assertEquals(4, (s2Var.value as List<*>).size)
-//        val s2Vars = s2Var.value as List<LiveVariable>
-//        val course = s2Vars.find { it.name == "course" }!!
-//        assertEquals(2, (course.value as List<LiveVariable>).size)
-//        assertEquals(
-//            2,
-//            ((course.value as List<LiveVariable>)
-//                .find { it.name == "time" }!!.value as List<LiveVariable>).size
-//        )
-//
-//        val student2 = s2Vars.find { it.name == "student2" }!!
-//        assertEquals(3, (student2.value as List<LiveVariable>).size)
-//        assertEquals(
-//            2,
-//            ((student2.value as List<LiveVariable>)
-//                .find { it.name == "course" }!!.value as List<LiveVariable>).size
-//        )
-//        assertEquals(
-//            2,
-//            (((student2.value as List<LiveVariable>)
-//                .find { it.name == "course" }!!.value as List<LiveVariable>)
-//                .find { it.name == "time" }!!.value as List<LiveVariable>).size
-//        )
-//    }
 }
