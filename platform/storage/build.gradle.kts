293d7d1bc53631c76b31112ea969ea41908ecb7b
Brandon Fergerson <bfergerson@apache.org>
2022-07-15 15:26:31 +0400

    move test

--- platform/storage/build.gradle.kts
+++ platform/storage/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 val platformGroup: String by project
 val projectVersion: String by project
 val vertxVersion: String by project
+val jupiterVersion: String by project
 
 group = platformGroup
 version = project.properties["platformVersion"] as String? ?: projectVersion
@@ -24,10 +25,31 @@ configure<PublishingExtension> {
     }
 }
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation(project(":protocol"))
     implementation("io.vertx:vertx-core:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin:$vertxVersion")
     implementation("io.vertx:vertx-lang-kotlin-coroutines:$vertxVersion")
     implementation("io.vertx:vertx-redis-client:$vertxVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
+    testImplementation("io.vertx:vertx-junit5:$vertxVersion")
+    testImplementation("io.vertx:vertx-web-client:$vertxVersion")
+}
+
+tasks.getByName<Test>("test") {
+    failFast = true
+    useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+        setExceptionFormat("full")
+
+        outputs.upToDateWhen { false }
+        showStandardStreams = true
+    }
 }
