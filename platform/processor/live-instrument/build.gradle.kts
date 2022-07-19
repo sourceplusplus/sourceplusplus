479f0679bcf45e17e0a94bb200be790161d63b37
Brandon Fergerson <bfergerson@apache.org>
2022-07-19 23:05:19 +0400

    remove mvn publish on processor

--- platform/processor/live-instrument/build.gradle.kts
+++ platform/processor/live-instrument/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm")
-    id("maven-publish")
 }
 
 val processorGroup: String by project
@@ -8,37 +7,6 @@ val projectVersion: String by project
 
 group = processorGroup
 version = project.properties["processorVersion"] as String? ?: projectVersion
-
-repositories {
-    mavenCentral()
-    maven(url = "https://jitpack.io")
-    maven(url = "https://pkg.sourceplus.plus/sourceplusplus/protocol")
-}
-
-configure<PublishingExtension> {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/sourceplusplus/processor-live-instrument")
-            credentials {
-                username = System.getenv("GH_PUBLISH_USERNAME")?.toString()
-                password = System.getenv("GH_PUBLISH_TOKEN")?.toString()
-            }
-        }
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = processorGroup
-                artifactId = "processor-live-instrument"
-                version = project.version.toString()
-
-                artifact("$buildDir/libs/spp-processor-live-instrument-${project.version}.jar")
-            }
-        }
-    }
-}
 
 dependencies {
     compileOnly(project(":platform:common"))
--- platform/processor/live-view/build.gradle.kts
+++ platform/processor/live-view/build.gradle.kts
@@ -8,12 +8,6 @@ val projectVersion: String by project
 group = processorGroup
 version = project.properties["processorVersion"] as String? ?: projectVersion
 
-repositories {
-    mavenCentral()
-    maven(url = "https://jitpack.io")
-    maven(url = "https://pkg.sourceplus.plus/sourceplusplus/protocol")
-}
-
 dependencies {
     compileOnly(project(":platform:common"))
 }
