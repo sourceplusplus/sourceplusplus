af6ea8d5b69aa9c31e18bc9cbb3d7cefcd3f92b9
Brandon Fergerson <bfergerson@apache.org>
2022-07-21 16:46:05 +0400

    fixes #424

--- platform/core/build.gradle.kts
+++ platform/core/build.gradle.kts
@@ -119,3 +119,4 @@ tasks.getByName<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("sha
     configurations.add(project.configurations.runtimeClasspath.get())
     configurations.add(project.configurations.shadow.get())
 }
+tasks.getByName("assemble").dependsOn("shadowJar")
