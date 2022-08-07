09c9ba107d88197b92b3dae93e9e2eb11a3a81d4
Brandon Fergerson <bfergerson@apache.org>
2022-08-07 14:02:48 +0400

    style: generate license header

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -113,7 +113,34 @@ subprojects {
 
     spotless {
         kotlin {
-            licenseHeaderFile(File(project.rootDir, "LICENSE-HEADER.txt"))
+            val startYear = 2022
+            val currentYear = java.util.Calendar.getInstance().get(java.util.Calendar.YEAR)
+            val copyrightYears = if (startYear == currentYear) {
+                "$startYear"
+            } else {
+                "$startYear-$currentYear"
+            }
+            licenseHeader(
+                """
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) $copyrightYears CodeBrig, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+                """.trimIndent()
+            )
         }
     }
 
