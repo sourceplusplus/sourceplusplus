bbbcd6e488b827be42d8738ac6021525315174ed
Brandon Fergerson <bfergerson@apache.org>
2022-08-07 14:25:54 +0400

    style: generate license header

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -120,27 +120,28 @@ subprojects {
             } else {
                 "$startYear-$currentYear"
             }
-            licenseHeader(
-                """
-/*
- * Source++, the open-source live coding platform.
- * Copyright (C) $copyrightYears CodeBrig, Inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published
- * by the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
-                """.trimIndent()
-            )
+
+            val licenseHeader = Regex("(    <one line[\\S\\s]+If not.+)")
+                .find(File(project.rootDir, "LICENSE").readText())!!.value.lines().joinToString("\n") {
+                    if (it.trim().isEmpty()) {
+                        " *"
+                    } else {
+                        " * " + it.trim()
+                    }
+                }
+            val formattedLicenseHeader = buildString {
+                append("/*\n")
+                append(
+                    licenseHeader.replace(
+                        "<one line to give the program's name and a brief idea of what it does.>",
+                        "Source++, the open-source live coding platform."
+                    )
+                        .replace("<year>", copyrightYears)
+                        .replace(" <name of author>", "CodeBrig, Inc.")
+                )
+                append("\n */")
+            }
+            licenseHeader(formattedLicenseHeader)
         }
     }
 
