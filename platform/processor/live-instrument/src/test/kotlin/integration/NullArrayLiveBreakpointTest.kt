b9fd4062824f2df38fdd98c7a93d7f03cfb54f30
Brandon Fergerson <bfergerson@apache.org>
2022-08-15 16:00:30 +0400

    test: null live var arrays

---
+++ platform/processor/live-instrument/src/test/kotlin/integration/NullArrayLiveBreakpointTest.kt
@@ -0,0 +1,111 @@
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package integration
+
+import io.vertx.core.json.Json
+import io.vertx.core.json.JsonObject
+import io.vertx.junit5.VertxTestContext
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import spp.protocol.SourceServices.Provide.toLiveInstrumentSubscriberAddress
+import spp.protocol.instrument.LiveBreakpoint
+import spp.protocol.instrument.LiveSourceLocation
+import spp.protocol.instrument.event.LiveInstrumentEvent
+import spp.protocol.instrument.event.LiveInstrumentEventType
+import spp.protocol.marshall.ProtocolMarshaller
+import java.util.concurrent.TimeUnit
+
+@Suppress("unused", "UNUSED_VARIABLE")
+class NullArrayLiveBreakpointTest : LiveInstrumentIntegrationTest() {
+
+    private fun nullArray() {
+        val activeSpan = startEntrySpan("nullArray")
+        val nullArray = arrayOfNulls<Any?>(10)
+        addLineLabel("done") { Throwable().stackTrace[0].lineNumber }
+        stopSpan(activeSpan)
+    }
+
+    @Test
+    fun `null array`() {
+        setupLineLabels {
+            nullArray()
+        }
+
+        val testContext = VertxTestContext()
+        val consumer = vertx.eventBus().consumer<Any>(toLiveInstrumentSubscriberAddress("system"))
+        consumer.handler {
+            val event = Json.decodeValue(it.body().toString(), LiveInstrumentEvent::class.java)
+            if (event.eventType == LiveInstrumentEventType.BREAKPOINT_HIT) {
+                //verify live breakpoint data
+                val bpHit = ProtocolMarshaller.deserializeLiveBreakpointHit(JsonObject(event.data))
+                testContext.verify {
+                    assertTrue(bpHit.stackTrace.elements.isNotEmpty())
+                    val topFrame = bpHit.stackTrace.elements.first()
+                    assertEquals(2, topFrame.variables.size)
+
+                    //nullArray
+                    val nullArrayVariable = topFrame.variables.first { it.name == "nullArray" }
+                    assertNotNull(nullArrayVariable)
+                    assertEquals(
+                        "java.lang.Object[]",
+                        nullArrayVariable.liveClazz
+                    )
+                    assertArrayEquals(
+                        arrayOfNulls<Any?>(10),
+                        (nullArrayVariable.value as List<Map<*, *>>).map { it["value"] }.toTypedArray()
+                    )
+                }
+
+                //test passed
+                testContext.completeNow()
+            }
+        }.completionHandler {
+            if (it.failed()) {
+                testContext.failNow(it.cause())
+                return@completionHandler
+            }
+
+            //add live breakpoint
+            instrumentService.addLiveInstrument(
+                LiveBreakpoint(
+                    location = LiveSourceLocation(
+                        NullArrayLiveBreakpointTest::class.qualifiedName!!,
+                        getLineNumber("done"),
+                        //"spp-test-probe" //todo: impl this so applyImmediately can be used
+                    ),
+                    //applyImmediately = true //todo: can't use applyImmediately
+                )
+            ).onSuccess {
+                //trigger live breakpoint
+                vertx.setTimer(5000) { //todo: have to wait since not applyImmediately
+                    nullArray()
+                }
+            }.onFailure {
+                testContext.failNow(it)
+            }
+        }
+
+        if (testContext.awaitCompletion(30, TimeUnit.SECONDS)) {
+            if (testContext.failed()) {
+                throw testContext.causeOfFailure()
+            }
+        } else {
+            throw RuntimeException("Test timed out")
+        }
+    }
+}
