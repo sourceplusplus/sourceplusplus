fa69c2ad44e3e08266dcadc9e4670541179e7d83
Brandon Fergerson <brandon.fergerson@codebrig.com>
2022-08-27 00:34:38 +0400

    test: atomic values live variables (#577)

---
+++ platform/processor/live-instrument/src/test/kotlin/integration/AtomicValueLiveBreakpointTest.kt
@@ -0,0 +1,133 @@
+/*
+ * Source++, the open-source live coding platform.
+ * Copyright (C) 2022 CodeBrig, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package integration
+
+import io.vertx.core.json.JsonObject
+import io.vertx.junit5.VertxTestContext
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import spp.protocol.instrument.LiveBreakpoint
+import spp.protocol.instrument.LiveSourceLocation
+import spp.protocol.marshall.ProtocolMarshaller
+import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.atomic.AtomicReference
+
+@Suppress("unused", "UNUSED_VARIABLE")
+class AtomicValueLiveBreakpointTest : LiveInstrumentIntegrationTest() {
+
+    private fun atomicValue() {
+        startEntrySpan("atomicValue")
+        val atomicMap = AtomicReference(mapOf("test" to "test"))
+        val atomicString = AtomicReference<String>().apply { set("test") }
+        val atomicInteger = AtomicInteger(1)
+        addLineLabel("done") { Throwable().stackTrace[0].lineNumber }
+        stopSpan()
+    }
+
+    @Test
+    fun `atomic value`() {
+        setupLineLabels {
+            atomicValue()
+        }
+
+        val testContext = VertxTestContext()
+        onBreakpointHit { bpHit ->
+            testContext.verify {
+                assertTrue(bpHit.stackTrace.elements.isNotEmpty())
+                val topFrame = bpHit.stackTrace.elements.first()
+                assertEquals(4, topFrame.variables.size)
+
+                //atomicMap
+                val atomicMapVariable = topFrame.variables.first { it.name == "atomicMap" }
+                assertNotNull(atomicMapVariable)
+                assertEquals(
+                    "java.util.concurrent.atomic.AtomicReference",
+                    atomicMapVariable.liveClazz
+                )
+                assertNotNull(atomicMapVariable.liveIdentity)
+                val atomicMapValue = ProtocolMarshaller.deserializeLiveVariable(
+                    JsonObject.mapFrom((atomicMapVariable.value as List<*>).first())
+                )
+                assertNotNull(atomicMapValue)
+                assertEquals(
+                    "java.util.Collections\$SingletonMap",
+                    atomicMapValue.liveClazz
+                )
+                val atomicMapFinalValue = ProtocolMarshaller.deserializeLiveVariable(
+                    JsonObject.mapFrom((atomicMapValue.value as List<*>).first())
+                )
+                assertNotNull(atomicMapFinalValue)
+                assertEquals("test", atomicMapFinalValue.name)
+                assertEquals("test", atomicMapFinalValue.value)
+
+                //atomicString
+                val atomicStringVariable = topFrame.variables.first { it.name == "atomicString" }
+                assertNotNull(atomicStringVariable)
+                assertEquals(
+                    "java.util.concurrent.atomic.AtomicReference",
+                    atomicStringVariable.liveClazz
+                )
+                assertNotNull(atomicStringVariable.liveIdentity)
+                val atomicStringValue = ProtocolMarshaller.deserializeLiveVariable(
+                    JsonObject.mapFrom((atomicStringVariable.value as List<*>).first())
+                )
+                assertNotNull(atomicStringValue)
+                assertEquals("test", atomicStringValue.value)
+
+                //atomicInteger
+                val atomicIntegerVariable = topFrame.variables.first { it.name == "atomicInteger" }
+                assertNotNull(atomicIntegerVariable)
+                assertEquals(
+                    "java.util.concurrent.atomic.AtomicInteger",
+                    atomicIntegerVariable.liveClazz
+                )
+                assertNotNull(atomicIntegerVariable.liveIdentity)
+                assertEquals(1, atomicIntegerVariable.value)
+            }
+
+            //test passed
+            testContext.completeNow()
+        }.completionHandler {
+            if (it.failed()) {
+                testContext.failNow(it.cause())
+                return@completionHandler
+            }
+
+            //add live breakpoint
+            instrumentService.addLiveInstrument(
+                LiveBreakpoint(
+                    location = LiveSourceLocation(
+                        AtomicValueLiveBreakpointTest::class.qualifiedName!!,
+                        getLineNumber("done"),
+                        //"spp-test-probe" //todo: impl this so applyImmediately can be used
+                    ),
+                    //applyImmediately = true //todo: can't use applyImmediately
+                )
+            ).onSuccess {
+                //trigger live breakpoint
+                vertx.setTimer(5000) { //todo: have to wait since not applyImmediately
+                    atomicValue()
+                }
+            }.onFailure {
+                testContext.failNow(it)
+            }
+        }
+
+        errorOnTimeout(testContext)
+    }
+}
