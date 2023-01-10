00d3609db956d20f02b64d24d85fc4eff201bf20
Brandon Fergerson <bfergerson@apache.org>
2023-01-10 21:54:02 +0400

    test: clean

--- platform/common/src/test/kotlin/integration/PlatformIntegrationTest.kt
+++ platform/common/src/test/kotlin/integration/PlatformIntegrationTest.kt
@@ -59,19 +59,6 @@ open class PlatformIntegrationTest {
                     "u5TQSJZX2U6YmpHlRVpDjs8g8EThjta7DSkmCNEPfn0YU_Cx9cSphEQkuZdyu_C8rPGzMDqyziOu1yWZYfFxK5SVMsGjfIOeB4" +
                     "qEDARssQ8-oREcp52Q4MmmT8d0oN4I4Fm_aa9X-R6dDkeWrFGrzcVEL1o"
 
-        //todo: generate
-        @Deprecated("To be removed")
-        const val TEST_JWT_TOKEN =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJkZXZlbG9wZXJfaWQiOiJ0ZXN0IiwiY3JlYXRlZF9hdCI6MTY1NzAzOTc1ODM5MiwiZ" +
-                    "XhwaXJlc19hdCI6MTY4ODU3NTc1ODM5MiwiaWF0IjoxNjU3MDM5NzU4fQ.h-fkoLkMqphwMVeXIz9weXDYe3NgLrIIWYZcEGLD" +
-                    "0DOvjlRGhXVPte2E6o_IJCUWys4g3V4TbT9LCdVTAt9ay8850ivlfAEnL-rOt3gaKxZF0AOxfdBQCb6PODOkHULhEMuM3Cy9oL" +
-                    "g7BRu8dZFoMRdbwWslihsLk9bXDyPCcuNKjdcBr8n_fFfbGUrO0HdWIhGEj7qnujzFbPk2m4CbnMTcgHXxTYma8uaxpa03EU2p" +
-                    "fZbQiauIHATsmvUvtkoGCnR5W0Vpz7M-1qo1JNphEMfxrsa_-8mbCkjG7rEXpWFvWfW6FwObKT3xs5D8jRpAmX0kCyfe9lPZ2e" +
-                    "J-Rdhy95f6LpjJoXkaPLGVQpU_lsPUk8yb410s2dTkY47FzZojTtY6gfl3IHDXnkG8FcaV6qqbTpTyz16rN9CxAynrg-7QsYdr" +
-                    "cZpT8A-6cjHuCJabgFxqKNm7Jfknu_f34q-_rZJpVyeC_2AYDETEB3AwCiWoTAurTAhPF4ZoMbfkie75TTF3Hjhbimw7PnzHg2" +
-                    "WbvSQpHyTR96rII4rjpqtFwC49CmPOCJPxLpEpa5rMW3gbvKa-M1IJRSnKN0rsPF1Yr80LIl4x1w6JZp47_MoYPe0rWNPojnBY" +
-                    "KP-1-SRBc1skSsijxtOWiYoz2UbA9xIDWUrWh6g-wUFa-xE4eAjFKgE"
-
         private var vertx: Vertx? = null
         val platformHost = System.getenv("SPP_PLATFORM_HOST") ?: "localhost"
 
