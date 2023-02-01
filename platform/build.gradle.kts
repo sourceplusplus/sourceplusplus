d0d4234c63827cee4086eb9b6b0936e23f4d8112
Brandon Fergerson <brandon.fergerson@codebrig.com>
2023-02-01 20:10:04 +0400

    revert: dependency bump

--- platform/build.gradle.kts
+++ platform/build.gradle.kts
@@ -51,7 +51,7 @@ subprojects {
         compileOnly("org.slf4j:slf4j-api:$slf4jVersion")
         implementation("io.vertx:vertx-tcp-eventbus-bridge:$vertxVersion")
         implementation("io.vertx:vertx-dropwizard-metrics:$vertxVersion")
-        implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
+        implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8:$jacksonVersion")
         implementation("com.fasterxml.jackson.datatype:jackson-datatype-guava:$jacksonVersion")
