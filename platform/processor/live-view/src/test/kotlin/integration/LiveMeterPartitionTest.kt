656bdaec4479ee389619ef9873e05afccae3246f
Brandon Fergerson <bfergerson@apache.org>
2023-06-27 18:23:15 -0500

    test: isolate

--- platform/processor/live-view/src/test/kotlin/integration/LiveMeterPartitionTest.kt
+++ platform/processor/live-view/src/test/kotlin/integration/LiveMeterPartitionTest.kt
@@ -26,6 +26,7 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.parallel.Isolated
 import spp.protocol.instrument.LiveMeter
 import spp.protocol.instrument.location.LiveSourceLocation
 import spp.protocol.instrument.meter.*
@@ -36,6 +37,7 @@ import spp.protocol.view.LiveViewEvent
 import spp.protocol.view.rule.RulePartition
 import spp.protocol.view.rule.ViewRule
 
+@Isolated
 class LiveMeterPartitionTest : LiveInstrumentIntegrationTest() {
 
     @Suppress("UNUSED_VARIABLE")
