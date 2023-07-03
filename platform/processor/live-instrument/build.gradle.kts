a6f5c4c1100c74ba8538db5ee2d36473ef6a67a8
Brandon Fergerson <bfergerson@apache.org>
2023-07-03 14:39:19 -0500

    fix: vcs test

--- platform/processor/live-instrument/build.gradle.kts
+++ platform/processor/live-instrument/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 val platformGroup: String by project
 val projectVersion: String by project
+val skywalkingAgentVersion: String by project
 
 group = platformGroup
 version = project.properties["platformVersion"] as String? ?: projectVersion
@@ -37,6 +38,9 @@ configure<PublishingExtension> {
 dependencies {
     compileOnly(project(":platform:common"))
     compileOnly(project(":platform:storage"))
+    testCompileOnly("org.apache.skywalking:apm-agent-core:$skywalkingAgentVersion") {
+        isTransitive = false
+    }
 
     testImplementation(project(":probes:jvm:boot"))
     testCompileOnly(project(":probes:jvm:common"))
