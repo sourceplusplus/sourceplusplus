9ceddbd59f7afdaf5d92a8ad3078d671ae34ab9f
Brandon Fergerson <bfergerson@apache.org>
2023-07-05 15:51:22 -0500

    ref: sourceplusplus/sourceplusplus#891

--- platform/processor/live-view/src/main/kotlin/spp/processor/view/ViewProcessor.kt
+++ platform/processor/live-view/src/main/kotlin/spp/processor/view/ViewProcessor.kt
@@ -165,11 +165,11 @@ object ViewProcessor : FeedbackProcessor() {
 
         val action = msg.headers().get("action")
         if (action == "addLiveView") {
-            if (failsPermissionCheck(RolePermission.ADD_LIVE_VIEW_SUBSCRIPTION)) return
+            if (failsPermissionCheck(RolePermission.ADD_LIVE_VIEW)) return
         } else if (action == "getLiveViews" || action == "getHistoricalMetrics") {
-            if (failsPermissionCheck(RolePermission.GET_LIVE_VIEW_SUBSCRIPTIONS)) return
+            if (failsPermissionCheck(RolePermission.GET_LIVE_VIEWS)) return
         } else if (action == "clearLiveViews") {
-            if (failsPermissionCheck(RolePermission.REMOVE_LIVE_VIEW_SUBSCRIPTION)) return
+            if (failsPermissionCheck(RolePermission.REMOVE_LIVE_VIEW)) return
         } else if (RolePermission.fromString(action) != null) {
             val necessaryPermission = RolePermission.fromString(action)!!
             if (selfInfo.permissions.contains(necessaryPermission)) {
